97260640142d10020a7d9b9e99c646ddfe0c07d8
Benjamin Wilson <35935536+bennywilson@users.noreply.github.com>
2022-01-12 11:55:09 -0500

    Giving AI weapons

--- Source/Oxi/OxiWeapon.cpp
+++ Source/Oxi/OxiWeapon.cpp
@@ -9,8 +9,9 @@
 AOxiWeapon::AOxiWeapon()
 {
 	Mesh1P = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("WeaponComponent"));
+	SetRootComponent(Mesh1P);
 	Mesh1P->SetOnlyOwnerSee(false);
-	Mesh1P->SetupAttachment(RootComponent);
+	//Mesh1P->SetupAttachment(RootComponent);
 	Mesh1P->bCastDynamicShadow = false;
 	Mesh1P->CastShadow = false;
 	//Mesh1P->SetRelativeRotation(FRotator(1.9f, -19.19f, 5.2f));
